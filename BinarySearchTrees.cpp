bdb0f1d1e278929dba62bf97c13b6f9a6c498c9c
Владислав Яшаров <vladislavyasharov@MacBook-Air-Vladislav.local>
2020-02-24 11:58:59 +0300

    Hello

--- BinarySearchTrees.cpp
+++ BinarySearchTrees.cpp
@@ -81,9 +81,6 @@ private:
                 father->left = NULL;
             } else if (father->right == target) {
                 father->right = NULL;
-            } else {
-                cout << "error 1582497703";
-                return;
             }
             SearchTree::freeMemory(target);
             return;
@@ -96,9 +93,6 @@ private:
                 father->left = target->right;
             } else if (father->right == target) {
                 father->right = target->right;
-            } else {
-                cout << "error 1582497728";
-                return;
             }
             target->right = NULL;
             SearchTree::freeMemory(target);
@@ -112,9 +106,6 @@ private:
                 father->left = target->left;
             } else if (father->right == target) {
                 father->right = target->left;
-            } else {
-                cout << "error 1582497756";
-                return;
             }
             target->left = NULL;
             SearchTree::freeMemory(target);
@@ -122,21 +113,28 @@ private:
         }
         
         PElem smallestOnTheRight = target->right;
+
         PElem fatherOfSmallestOnTheRight = target;
+
         while (smallestOnTheRight->left != NULL) {
+
             fatherOfSmallestOnTheRight = smallestOnTheRight;
+
             smallestOnTheRight = smallestOnTheRight->left;
         }
         
         if (fatherOfSmallestOnTheRight == NULL) {
+            
             root->right = smallestOnTheRight->right;
+
         } else if (fatherOfSmallestOnTheRight->left == smallestOnTheRight) {
+            
             fatherOfSmallestOnTheRight->left = smallestOnTheRight->right;
+
         } else if (fatherOfSmallestOnTheRight->right == smallestOnTheRight) {
+            
             fatherOfSmallestOnTheRight->right = smallestOnTheRight->right;
-        } else {
-            cout << "error 1582497773";
-            return;
+
         }
         
         smallestOnTheRight->left = target->left;
@@ -148,9 +146,6 @@ private:
             father->left = smallestOnTheRight;
         } else if (father->right == target) {
             father->right = smallestOnTheRight;
-        } else {
-            cout << "error 1582497779";
-            return;
         }
         
         target->left = NULL;
@@ -168,15 +163,6 @@ private:
         }
         
         return 1 + max(SearchTree::getHeight(p->left), SearchTree::getHeight(p->right));
-    }
-    
-    static void fillTiers(const PElem &p, int current_position, int * array_of_tiers) {
-        if (p == NULL) {
-            return;
-        }
-        array_of_tiers[current_position] = p->data;
-        SearchTree::fillTiers(p->left, 2*current_position, array_of_tiers);
-        SearchTree::fillTiers(p->right, 2*current_position + 1, array_of_tiers);
     }
     
     static void fillTiersMoreBeautiful(const PElem &p, int shift, int height, int * array_of_tiers) {
@@ -239,37 +225,6 @@ public:
         SearchTree::removeElement(root, n);
     }
     
-    void printTiers() {
-        int h = SearchTree::getHeight(root);
-        if (h == 0) {
-            cout << "tree is empty" << endl;
-        }
-        
-        int total_length = pow(2, h);
-        int tiers[total_length];
-        int dummy_value = -1;
-        for (int i = 0; i < total_length; i++) {
-            tiers[i] = dummy_value;
-        }
-        
-        SearchTree::fillTiers(root, 1, tiers);
-        
-        const char free_space_character = '-';
-
-        for (int i = 0; i < h; i++) {
-            int t = pow(2, i);
-            for (int j = 0; j < t; j++) {
-                if (tiers[t + j] == dummy_value) {
-                    cout << free_space_character;
-                } else {
-                    cout << tiers[t + j];
-                }
-                cout << string(pow(2, h-i+1)-1, free_space_character);
-            }
-            cout << endl;
-        }
-    }
-    
     void printTiersMoreBeautiful() {
         int h = SearchTree::getHeight(root);
         if (h == 0) {
@@ -310,7 +265,6 @@ public:
              << "p - to print SearchTree in visual form" << endl
              << "i - to print SearchTree in increasing order" << endl
              << "d - to print SearchTree in decreasing order" << endl
-             << "u - to print SearchTree in ugly visual form" << endl
              << "h - to see this help message" << endl
              << "q - to quit" << endl
         ;
@@ -375,9 +329,6 @@ int main() {
                 break;
             case 'd':
                 searchTree.decreasingPrint();
-                break;
-            case 'u':
-                searchTree.printTiers();
                 break;
             case 'h':
                 SearchTree::printHelp();
